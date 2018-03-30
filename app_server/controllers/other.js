0198150e485d05da79bf64eb0995a297b52fe1e6
jekakmail <jekakmail@gmail.com>
2018-03-31 00:13:35 +0300

    + add resources + add images * edit routes * implement layout view * implement main view

--- app_server/controllers/other.js
+++ app_server/controllers/other.js
@@ -1,5 +1,5 @@
 module.exports.contacts = function(req, res) {
-    res.render('index', { title: 'Contacts' });
+    res.render('contacts', { title: 'Contacts' });
 };
 
 module.exports.about = function(req, res) {

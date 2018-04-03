6f5ccf7be63d1f86825a04fde245b9e1771939b9
jekakmail <jekakmail@gmail.com>
2018-04-04 00:51:22 +0300

    + link price data from api to price controller * refactor price view for api data + add price file

--- app_server/controllers/price.js
+++ app_server/controllers/price.js
@@ -1,9 +1,14 @@
-var renderPricepage = function (req, res) {
+var request = require('request');
+var apiOptions = {
+    server: "http://localhost:3000"
+}
+
+var renderPricepage = function (req, res, responseBody) {
     res.render('price', {
         title: 'Отделочные работы в Минске',
         siteName: 'StroiLux.by',
         header: 'Цены',
-        landHeader: 'Расценки на отделочные работы 2017 год',
+        landHeader: 'Расценки на отделочные работы 2018 год',
         taxInfo: {
             name: 'Климович Р.А.',
             state: 'ИП',
@@ -11,6 +16,8 @@ var renderPricepage = function (req, res) {
             scanDocument: '/images/img1.jpg',
             linkToProfile: 'https://s.onliner.by/profiles/39974'
         },
+        priceData: responseBody,
+        priceLink: '/resources/PriceOfWork.xlsx',
         about: {
             header: 'О нас',
             body: 'Большой опыт. Договор. Гарантия. Выезд мастера на обмер и составление сметы бесплатно. Все пыльные работы выполняются только с промышленным пылесосом, что гарантирует чистоту в помещении. Использование профессионального инструмента. Помощь в выборе материалов, консультация.'
@@ -19,5 +26,14 @@ var renderPricepage = function (req, res) {
 }
 
 module.exports.index = function (req, res) {
-    renderPricepage(req, res);
+    var requestOptions, path;
+    path = '/api/price';
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: 'GET',
+        json: {}
+    }
+    request(requestOptions, function (err, response, body) {
+        renderPricepage(req, res, body);
+    })
 };
